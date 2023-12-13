7f3a7bd2217afc6ae46379785a3dccf0b55818b1
Fayth7 <flexflakes@gmail.com>
2023-12-13 17:43:56 +0000

    task 4

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,11 +1,6 @@
 -- List bands with Glam rock as their main style, ranked by longevity
-SELECT band_name,
-       IF(splitted[2] != '0000', formed, IF(splitted[1] != '0000', splitted[1], 2022)) as lifespan
-FROM (
-    SELECT band_name,
-           SPLIT_STR(formed, '-', 3) as formed,
-           SPLIT_STR(split, '-', 3) as split
-    FROM bands
-    WHERE style = 'Glam rock'
-) AS glam_bands
+
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
+FROM metal_bands
+WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
 ORDER BY lifespan DESC;
