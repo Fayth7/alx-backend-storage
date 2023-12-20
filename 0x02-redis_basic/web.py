0fc7386dab5992324b4c43d6d849119072413a2d
Fayth7 <flexflakes@gmail.com>
2023-12-20 16:47:29 +0000

    updates to web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,50 +1,30 @@
 #!/usr/bin/env python3
 """ Redis Module """
 
-import requests
-import redis
 from functools import wraps
+import redis
+import requests
 from typing import Callable
 
-# Create a Redis connection
 redis_client = redis.Redis()
 
-def count_accesses(url: str) -> int:
-    count_key = f"count:{url}"
-    return redis_client.incr(count_key)
+def count_requests(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(url: str) -> str:
+        count_key = f"count:{url}"
+        redis_client.incr(count_key)
 
-def cache_page(url: str, content: str, expiration_time: int = 10):
-    cache_key = f"cache:{url}"
-    redis_client.setex(cache_key, expiration_time, content)
+        cached_html = redis_client.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
 
+        html = method(url)
+        redis_client.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapper
+
+@count_requests
 def get_page(url: str) -> str:
-    # Check if the content is cached
-    cache_key = f"cache:{url}"
-    cached_content = redis_client.get(cache_key)
-    if cached_content is not None:
-        # Content found in cache, return it
-        return cached_content.decode('utf-8')
-
-    # Content not in cache, make a request
-    response = requests.get(url)
-    content = response.text
-
-    # Cache the content and track the access count
-    count = count_accesses(url)
-    cache_page(url, content)
-
-    print(f"Accessed {url} {count} times.")
-
-    return content
-
-# Example usage
-if __name__ == "__main__":
-    slow_url = "http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.google.com"
-    
-    # Access the slow URL multiple times
-    for _ in range(5):
-        get_page(slow_url)
-
-    # Access a normal URL
-    normal_url = "https://www.example.com"
-    get_page(normal_url)
+    req = requests.get(url)
+    return req.text
