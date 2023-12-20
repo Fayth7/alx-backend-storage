6ae9084af7174e892fdeacd87a9383028bd4867b
Fayth7 <flexflakes@gmail.com>
2023-12-20 16:41:26 +0000

    updates

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,80 +1,64 @@
 #!/usr/bin/env python3
 
+from typing import Callable, Optional, Union
+from uuid import uuid4
 import redis
-import uuid
-from typing import Callable
 from functools import wraps
+
+def count_calls(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = f"{method.__qualname__}:calls"
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+def call_history(method: Callable) -> Callable:
+    key = method.__qualname__
+    inputs_key, outputs_key = f"{key}:inputs", f"{key}:outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        self._redis.rpush(inputs_key, str(args))
+        result = method(self, *args, **kwargs)
+        self._redis.rpush(outputs_key, str(result))
+        return result
+
+    return wrapper
+
+def replay(method: Callable) -> None:
+    name = method.__qualname__
+    cache = redis.Redis()
+    calls_key = f"{name}:calls"
+    calls = cache.get(calls_key).decode("utf-8")
+    
+    print(f"{name} was called {calls} times:")
+    
+    inputs = cache.lrange(f"{name}:inputs", 0, -1)
+    outputs = cache.lrange(f"{name}:outputs", 0, -1)
+    
+    for inp, out in zip(inputs, outputs):
+        print(f"{name}(*{inp.decode('utf-8')}) -> {out.decode('utf-8')}")
 
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @staticmethod
-    def count_calls(method: Callable) -> Callable:
-        key = method.__qualname__
-
-        @wraps(method)
-        def wrapper(self, *args, **kwargs):
-            # Increment count in Redis
-            count_key = f"{key}_count"
-            current_count = self._redis.incr(count_key)
-
-            # Call the original method and return its result
-            result = method(self, *args, **kwargs)
-            return result
-
-        return wrapper
-
-    @staticmethod
-    def call_history(method: Callable) -> Callable:
-        key = method.__qualname__
-
-        @wraps(method)
-        def wrapper(self, *args, **kwargs):
-            # Append input arguments to Redis list
-            inputs_key = f"{key}:inputs"
-            self._redis.rpush(inputs_key, str(args))
-
-            # Execute the wrapped function to retrieve the output
-            result = method(self, *args, **kwargs)
-
-            # Store the output in the Redis list
-            outputs_key = f"{key}:outputs"
-            self._redis.rpush(outputs_key, str(result))
-
-            return result
-
-        return wrapper
-
     @count_calls
     @call_history
-    def store(self, data):
-        key = str(uuid.uuid4())
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key = str(uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self, key, fn=None):
-        data = self._redis.get(key)
-        if data is None:
-            return None
-        return fn(data) if fn is not None else data
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        value = self._redis.get(key)
+        return fn(value) if fn else value
 
-def replay(func):
-    cache = Cache()
-    key = func.__qualname__
-    
-    # Retrieve inputs and outputs from Redis lists
-    inputs = cache._redis.lrange(f"{key}:inputs", 0, -1)
-    outputs = cache._redis.lrange(f"{key}:outputs", 0, -1)
+    def get_str(self, key: str) -> str:
+        return self.get(key, lambda value: value.decode('utf-8'))
 
-    print(f"{key} was called {len(inputs)} times:")
-    for inp, out in zip(inputs, outputs):
-        print(f"{key}(*{inp.decode('utf-8')}) -> {out.decode('utf-8')}")
-
-# Test cases
-cache = Cache()
-cache.store("foo")
-cache.store("bar")
-cache.store(42)
-replay(cache.store)
+    def get_int(self, key: str) -> int:
+        value = self.get(key, lambda value: int(value.decode('utf-8', 'ignore')) if value else 0)
+        return value
